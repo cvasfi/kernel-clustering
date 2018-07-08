678f70f402538c77caa4138f1cce19331897c723
cvasfi <yunus.ec@gmail.com>
2018-07-08 23:30:22 +0200

    Implemented conversion and inference for pytorch

---
+++ pytorch/AlexNet.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class AlexNet(nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
+
+
+
+class LookupConv(nn.Conv2d):
+    def __init__(self, indices, *args, **kwargs):
+        super(LookupConv, self).__init__(*args, **kwargs)
+        self.indices = indices
+
+    def forward(self, input):
+        x = super(LookupConv, self).forward(input)
+        return torch.index_select(x, 1, self.indices)
+
+
+class AlexNetLookup(nn.Module):
+
+    def __init__(self, indices, shrink=2, num_classes=1000):
+        super(AlexNetLookup, self).__init__()
+        self.features = nn.Sequential(
+            LookupConv(indices[0], 3, 96/shrink, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            LookupConv(indices[1], 96, 256/shrink, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            LookupConv(indices[2], 256, 384/shrink, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            LookupConv(indices[3], 384, 384/shrink, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            LookupConv(indices[4], 384, 256/shrink, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
---
+++ pytorch/clusternet.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import time
+import numpy as np
+import AlexNet
+import math
+from sklearn.cluster import KMeans
+import time
+
+class clusternet(object):
+    def __init__(self, modelpath=None, shrink=2):
+        self.in_net     = AlexNet.AlexNet()
+        self.in_params  = self.in_net.state_dict()
+        self.shrink     = shrink
+        self.imagenet_dummy=torch.rand(64,3,224,224)
+
+
+
+
+    def convert_network(self):
+
+        indices, codebooks = self.quantize_all_params()
+        codebooks=iter(codebooks)
+
+        qnet=AlexNet.AlexNetLookup(indices, shrink=self.shrink)
+        qnet_params=qnet.state_dict()
+
+        for k in qnet_params:
+            if "weight" in k and "features" in k:
+                qnet_params[k].data.copy_(next(codebooks))
+
+        return qnet
+
+
+
+
+
+    def get_quantized_filters(self, filters, shrink):
+        shape = filters.shape
+        n_clusters = shape[0] / shrink
+
+        filters_shaped = filters.reshape((shape[0], shape[1] * shape[2] * shape[3])).data.numpy()
+        estimator = KMeans(n_clusters=n_clusters)
+        estimator.fit(filters_shaped)
+
+        filter_kmean_indexes = estimator.predict(X=filters_shaped)
+        filters_quantized = np.array([estimator.cluster_centers_[idx] for idx in filter_kmean_indexes])
+
+        return torch.LongTensor(filter_kmean_indexes), torch.Tensor(estimator.cluster_centers_).reshape(n_clusters,shape[1],shape[2],shape[3]), torch.Tensor(filters_quantized).reshape(shape)
+
+
+    def quantize_all_params(self, debug=False):
+        in_params = self.in_params
+        indices_list = []
+        codebooks = []
+
+        for k in in_params:
+            if "weight" in k and "features" in k:
+                filters = in_params[k]
+                indices, codebook, q_filters = self.get_quantized_filters(filters, self.shrink)
+
+                indices_list.append(indices)
+                codebooks.append(codebook)
+
+                if debug:
+                    in_params[k].data.copy_(q_filters)
+                    assert torch.equal(in_params[k], q_filters)
+
+        return indices_list, codebooks
+
+
+    def forward_pass(self, net, data):
+        return net(data)
+
+    def sanity_test1(self):
+        res1=self.forward_pass(self.in_net, self.imagenet_dummy)
+        self.quantize_all_params(debug=True)
+
+        res2=self.forward_pass(self.in_net, self.imagenet_dummy)
+        err = torch.mean((res1 - res2) ** 2)
+
+        print err.data.numpy()
+
+
+    def sanity_test2(self):
+        begin=time.time()
+        res1=self.forward_pass(self.in_net, self.imagenet_dummy)
+        time1=time.time()-begin
+
+        qnet = self.convert_network()
+
+        begin=time.time()
+        res2=self.forward_pass(qnet, self.imagenet_dummy)
+        time2=time.time()-begin
+        err = torch.mean((res1 - res2) ** 2)
+
+        print "speedup is {}, mean err is {}".format(float(time1)/time2, err.data.numpy())
+
+
+cn=clusternet("/home/tapir/Documents/Thesis/kernel-clustering/notebooks/test.pt",shrink=4)
+
+cn.sanity_test2()
